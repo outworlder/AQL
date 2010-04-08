c083cb54d914902ee3b0599200d557f778bd0533
Stephen <spedrosa@gmail.com>
2010-04-08 18:57:27 -0300

    - Created the egg and support files.

--- aql.scm
+++ aql.scm
@@ -1,46 +1,50 @@
-(use srfi-1)                            ;For 'every'
+(module aql
+  (SELECT INSERT)
 
-;; TODO: Joins
-;; TODO: Where
-;; TODO: Order by
-;; TODO: LIMIT
-;; TODO: aggregation (count, all, sum, etc)
-;; TODO: Include field renaming
-(define (SELECT tables #!key (fields "*"))
-  (string-append "SELECT " (*result-fields* fields) " FROM " (*result-fields* tables)))
+  (import chicken scheme srfi-1 data-structures)
 
-(define (*result-fields* columns #!key quote-fields)
-  (if (list? columns)
-      (string-intersperse
-       (map (lambda (column)
-              (*quote* column quote-fields)) columns) ",")
-      (*quote* columns quote-fields)))
+  ;; TODO: Joins
+  ;; TODO: Where
+  ;; TODO: Order by
+  ;; TODO: LIMIT
+  ;; TODO: aggregation (count, all, sum, etc)
+  ;; TODO: Include field renaming
+  (define (SELECT tables #!key (fields "*"))
+    (string-append "SELECT " (*result-fields* fields) " FROM " (*result-fields* tables)))
 
-(define (*quote* field enabled)
-  (let ([value (->string field)])
-    (if (and (string? field) enabled)
-        (string-append "\"" value "\"")
-        value)))
+  (define (*result-fields* columns #!key quote-fields)
+    (if (list? columns)
+        (string-intersperse
+         (map (lambda (column)
+                (*quote* column quote-fields)) columns) ",")
+        (*quote* columns quote-fields)))
 
-(define (alist? list)
-  (and (list? list) (every pair? list)))
+  (define (*quote* field enabled)
+    (let ([value (->string field)])
+      (if (and (string? field) enabled)
+          (string-append "\"" value "\"")
+          value)))
 
-;; alist of field values or just the values
-(define (INSERT table values #!key fields (marks "?"))
-  (let ([fields-string (if fields
-                           (string-append "("
-                                          (*result-fields* fields)
-                                          ")")
-                           "")]
-        [values-string (string-append "("
-                                      (if (eqv? values '?)
-                                          (begin
-                                            (unless (list? fields)
-                                              (signal
-                                               (make-property-condition
-                                                'exn
-                                                'message
-                                                "If using markers, the list of fields must be informed."))))
-                                          (*result-fields* (map (lambda (value) marks) fields)))
-                                      (*result-fields* values quote-fields: #t)) ")")])
-  (string-append "INSERT INTO " (->string table) " " fields-string " VALUES " values-string))
+  (define (alist? list)
+    (and (list? list) (every pair? list)))
+
+  ;; alist of field values or just the values
+  (define (INSERT table values #!key fields (marks "?"))
+    (let ([fields-string (if fields
+                             (string-append "("
+                                            (*result-fields* fields)
+                                            ")")
+                             "")]
+          [values-string (string-append "("
+                                        (if (eqv? values '?)
+                                            (begin
+                                              (unless (list? fields)
+                                                (signal
+                                                 (make-property-condition
+                                                  'exn
+                                                  'message
+                                                  "If using markers, the list of fields must be informed.")))
+                                              (*result-fields* (map (lambda (value) marks) fields)))
+                                            (*result-fields* values quote-fields: #t)) ")")])
+    (string-append "INSERT INTO " (->string table) " " fields-string " VALUES " values-string)))
+  )                                     ; Module
